67686a4cbffb863d92dd3ef00e120fb0783cd2c7
Sahar <Aguidi53@gmail.com>
2023-06-08 12:14:31 -0700

    finished

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    import sys
+    num = 1
+    argc = len(sys.argv) - num
+    print('{} argument{}{}'.format(
+        argc, 's' * (argc != 1),
+        ':' if argc > 0 else '.'
+        ))
+    for arg in sys.argv[n:]:
+        print('{}: {}'.format(num, arg))
+        num += 1
