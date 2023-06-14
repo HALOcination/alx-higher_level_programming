faec3d1416aab24939ecd69fa3e6315691625739
Sahar <Aguidi53@gmail.com>
2023-06-14 11:30:03 -0700

    04

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-def print_sorted_dictionary(a_dictionary):
-	ord_keys = list(a_dictionary.keys())
-	ord_keys.sort()
-	for n in ord_keys:
-		print("{}: {}".format(n, a_dictionary.get(n)))
