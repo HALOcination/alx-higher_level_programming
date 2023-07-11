ad750dfc369a500990756fbe86b5b06ba5cc1623
Sahar <Aguidi53@gmail.com>
2023-07-11 11:35:59 -0700

    .

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,10 +4,6 @@
 
 def write_file(filename="", text=""):
     """writes a string to a text file (UTF8)
-
-    Args:
-    	filename : name of the file
-        text : text to write
     """
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
